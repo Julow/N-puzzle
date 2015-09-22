87d927c3327e527ad689980b9d43efd0aa6bff1d
jaguillo <juloo.dsi@gmail.com>
2015-09-22 18:58:08 +0200

    Thing about AView again

--- libftui/include/AView.hpp
+++ libftui/include/AView.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/22 12:56:29 by ngoguey           #+#    #+#             */
-/*   Updated: 2015/09/22 16:10:34 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/09/22 18:57:07 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,48 @@ public:
 	enum		Flags
 	{
 		HIDDEN = (1 << 1),
-		REQUEST_REDRAW = (1 << 2),
-		REQUEST_MEASURE = (1 << 3),
-		REQUEST_UPDATE = (1 << 4),
-		ENABLED_MOUSE = (1 << 5),
-		ENABLED_KEYBOARD = (1 << 7),
-		MOUSE_DOWN = (1 << 8),
+		MOUSE_DOWN = (1 << 2),
+	};
+	enum		Query
+	{
+		QUERY_REDRAW = (1 << 1),
+		QUERY_MEASURE = (1 << 2),
+		QUERY_UPDATE = (1 << 3),
+	};
+	enum		ActivationsDeMerde
+	{
+		ENABLED_MOUSE = (1 << 1),
+		ENABLED_KEYBOARD = (1 << 2),
 	};
 
 	AView();
 	virtual ~AView();
+
+	std::string const	*getId(void) const;
+
+	virtual void		setVisibility(bool visi);
+
+	bool				isVisible(void);
+	bool				isMouseDown(void);
+
+	bool				isMouseEnabled(void);
+	bool				isKeyboardEnabled(void);
+
+/*
+** Request
+*/
+	void				requestDimension(Vec2<int> d);
+
+protected:
+
+	std::string const * const	_id;
+
+	ALayout				*_parent;
+	IViewHolder			*_holder;
+
+	long				_flags;
+	long				_queries;
+	long				_active;
 
 /*
 ** High level callbacks
@@ -53,11 +85,6 @@ public:
 /*
 ** Low level callbacks
 */
-	virtual Vec2<int>	onMeasure(void);
-	virtual void		onUpdate(void); // ENABLED_*
-
-	virtual void		onDraw(ACanvas &canvas);
-
 	virtual void		onMouseMove(int x, int y);
 	virtual void		onMouseWheel(float delta);
 
@@ -68,26 +95,35 @@ public:
 	virtual bool		onKeyDown(int key_code);
 
 /*
-** Useless
+** Recursive callbacks
 */
-	virtual void		setVisibility(bool visi);
+	virtual void		onDraw(ACanvas &canvas);
 
-	bool				isVisible(void);
-	bool				isMouseDown(void);
+	virtual void		onSizeChange(void);
+	virtual void		onMeasure(void){return _holder.setRequestedDimension();}
+
+	virtual void		onUpdate(void); // ENABLED_*
 
 /*
-** Request
+** Query
 */
-	void				requestRedraw(void);
-	void				requestMeasure(void);
-	void				requestUpdate(void);
+/*
+	void				setFlagHidden(bool b);		//set que lui, call setflagredraw			util, state, pas important, a moi
+	void				setFlagsMouseDown(bool b);	//set que lui, ses parents se sont deja actives
 
-protected:
+	void				setFlagsRedraw(void);		//set lui, et ses parents	query
+	void				setFlagsMeasure(void);		//set lui, et ses parents
+	void				setFlagsUpdate(void);		//set lui, et ses parents
 
-	ALayout				*_parent;
-	IViewHolder			*_holder;
+	void				setFlagsEnableMouse(bool b);//set lui, et ses parents, set son activity		activer une feature enable
+	void				setFlagsKeyboard(bool b);	//set lui, et ses parents, set son activity		activer une feature	enable
+*/
+	void				queryRedraw(void);
+	void				queryMeasure(void);
+	void				queryUpdate(void);
 
-	long				_flags;
+	void				enableMouse(bool e);
+	void				enableKeyboard(bool e);
 
 private:
 	AView(AView const &src);
