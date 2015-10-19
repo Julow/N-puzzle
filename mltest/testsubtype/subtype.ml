8ba926906164f086531f4f8e62ade42e74c764bd
Ngoguey42 <ngoguey@student.42.fr>
2015-10-19 13:38:18 +0200

    improved inheritance, but not perfect yet

--- mltest/testsubtype/subtype.ml
+++ mltest/testsubtype/subtype.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/10/18 13:09:04 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/10/19 13:11:36 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/10/19 13:37:29 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -27,18 +27,6 @@ module GenericInterfaces =
 	  sig
 		include PATHFINDER
 		val solve : graph -> graph -> (graph -> int) -> graph list
-
-		module type STATE =
-		  sig
-		  	type t = {
-		  		graph	: graph;
-		  		g		: int;
-		  		h		: int;
-		  	  }
-			include ORDEREDTYPE
-					with type t := t
-		  end
-		(* module State : STATE *)
 	  end
   end(* END OF FILE ml *)
 
@@ -83,6 +71,63 @@ module AStar : ASTAR =
 	  end
 
 	let solve (grinit:graph) grgoal he =
+	  []
+
+  end(* END OF FILE ml *)
+
+module type IDASTAR =
+  sig(* FILE mli *)
+	include Grid.HEURISTIC_PATHFINDER
+
+  end(* END OF FILE mli *)
+
+
+let equal gra grb =
+  gra = grb
+
+let cost gra grb =
+  1
+
+let successors gr =
+  []
+
+module IDAStar : IDASTAR =
+  struct(* FILE ml *)
+	type graph = Grid.t
+
+	let solve grinit grgoal he =
+	  (* let bound = he grinit in *)
+
+	  let rec search gr g bound =
+		let f = g + (he gr) in
+		if f > bound then
+		  f
+		else if equal gr grgoal then
+		  42
+		else
+		  min_successor gr g bound
+	  and min_successor gr g bound =
+		let rec aux successors decr =
+		  match successors with
+		  | hd::tl			->
+			 let v = search hd (g + (cost gr hd)) bound in
+			 if v = 42
+			 then 42
+			 else aux tl (min v decr)
+		  | _				-> decr
+		in
+		aux (successors gr) max_int
+
+	  in
+
+	  let rec aux bound =
+		let threshold = search grinit 0 bound in
+		if threshold = 42
+		then ()
+		else aux threshold
+	  in
+	  aux (he grinit);
+
 	  []
 
   end(* END OF FILE ml *)
