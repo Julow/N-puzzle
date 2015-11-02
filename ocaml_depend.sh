11ea42682d5d95982ffde0343f48fddd1d6499f2
jaguillo <juloo.dsi@gmail.com>
2015-11-02 16:40:46 +0100

    Fix OCAML_OBJS order

--- ocaml_depend.sh
+++ ocaml_depend.sh
@@ -7,7 +7,7 @@
 #    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/11/02 13:29:23 by jaguillo          #+#    #+#              #
-#    Updated: 2015/11/02 16:08:44 by jaguillo         ###   ########.fr        #
+#    Updated: 2015/11/02 16:39:23 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,7 +17,7 @@ OCAML_SOURCES="`find $OCAML_DIR -name '*.ml*' -type f`"
 
 OCAML_OBJS=""
 
-for file_name in $OCAML_SOURCES; do
+for file_name in `ocamldep -sort $OCAML_SOURCES`; do
 	file_name=${file_name/.mli/.cmi}
 	file_name=${file_name/.ml/.cmx}
 	OCAML_OBJS="$OCAML_OBJS $file_name"
@@ -32,4 +32,4 @@ echo
 echo "OCAML_DIR = $OCAML_DIR"
 echo
 
-ocamldep $OCAML_SOURCES
+ocamldep -I "$OCAML_DIR" $OCAML_SOURCES
