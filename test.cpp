5557d26523a8048b5ff1fcf3243a729b5518b603
Ngoguey42 <ngoguey@student.42.fr>
2015-11-18 20:00:02 +0100

    updated modules/ftlua/callOverloads.xlsx

--- test.cpp
+++ test.cpp
@@ -2,19 +2,20 @@
 #include <type_traits>
 #include <cstdint>
 #include <iostream>
+typedef void		lua_State;
 
 namespace ftlua
 {
 
 // KEY
 template<typename... ARGS>
-class KeyWrapper
+class KeysWrapper
 {
 public:
-	KeyWrapper(ARGS const &...args)
+	KeysWrapper(ARGS const &...args)
 		: _tup{args...}
 		{ }
-	 ~KeyWrapper(void)
+	 ~KeysWrapper(void)
 		{ }
 
 private:
@@ -26,29 +27,30 @@ private:
 };
 
 template<typename... ARGS>
-KeyWrapper<ARGS...>	make_key(ARGS const &...args)
+KeysWrapper<ARGS...>	make_key(ARGS const &...args)
 {
-	return KeyWrapper<ARGS...>{args...};
+	return KeysWrapper<ARGS...>{args...};
 }
 
 //PUSH
 template<bool USELUAERR = false, class T>
-void			push(T const &)
+void			push(lua_State *l, T const &)
 {
 	static_assert(!std::is_same<T, T>::value, "No");
 	return ;
 }
 
 template<bool USELUAERR = false, class T = int>
-void			push(int const &i)
+void			push(lua_State *l, int const &i)
 {
 	std::cout << "push:" << i << '(' << USELUAERR << ')' << std::endl;
 
 	return ;
 }
 
-template<bool USELUAERR = false, typename... ARGS, class T = KeyWrapper<ARGS...> >
-void			push(KeyWrapper<ARGS...> const &wrap)
+template<bool USELUAERR = false
+		 , typename... ARGS, class T = KeysWrapper<ARGS...> >
+void			push(lua_State *l, KeysWrapper<ARGS...> const &wrap)
 {
 	std::cout << "salut" << std::endl;
 
@@ -59,21 +61,23 @@ typedef std::nullptr_t	nil_t;
 static nil_t			nil{};
 
 };
+
 int							main(void)
 {
-	void	*trux = (void*)0x42;
+	void			*trux = (void*)0x42;
+	lua_State		*l;
 
-	ftlua::push(42);
-	ftlua::push<true>(42);
-	ftlua::push<false>(42);
+	ftlua::push(l, 42);
+	ftlua::push<true>(l, 42);
+	ftlua::push<false>(l, 42);
 	// make_key(trux, "callbacks", 18);
-	ftlua::push(ftlua::make_key(trux, "callbacks", 18));
+	ftlua::push(l, ftlua::make_key(trux, "callbacks", 18));
 
 	// push(nil);
-	// make_asGKey(42);
-	// push(make_asGKey(42));
-	// push<true>(make_asGKey(42));
-	// push<false>(make_asGKey(42));
+	// make_asGKeys(42);
+	// push(make_asGKeys(42));
+	// push<true>(make_asGKeys(42));
+	// push<false>(make_asGKeys(42));
 
 	return (0);
 }
